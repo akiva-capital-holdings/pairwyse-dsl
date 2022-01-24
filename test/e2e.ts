abba6f2027a052c32ddc1a6aff18d69e89212512
Misha Kushka <kushka.misha@gmail.com>
2022-01-24 17:21:21 +0000

    fix: end-to-end test

--- test/e2e.ts
+++ test/e2e.ts
@@ -1,197 +1,153 @@
-// import { ethers } from "hardhat";
-// import { expect } from "chai";
-// import {
-//   AppParserMock, Context, Stack, StackValue__factory,
-// } from "../typechain";
-// import { checkStackTail, hex4Bytes, hex4BytesShort } from "./utils/utils";
+import { ethers } from "hardhat";
+import { expect } from "chai";
+import { AppMock, Context, Stack, StackValue__factory } from "../typechain";
+import { checkStack, hex4Bytes, hex4BytesShort } from "./utils/utils";
 
-// const NEXT_MONTH = Math.round((Date.now() + 1000 * 60 * 60 * 24 * 30) / 1000);
-// const PREV_MONTH = Math.round((Date.now() - 1000 * 60 * 60 * 24 * 30) / 1000);
+const NEXT_MONTH = Math.round((Date.now() + 1000 * 60 * 60 * 24 * 30) / 1000);
+const PREV_MONTH = Math.round((Date.now() - 1000 * 60 * 60 * 24 * 30) / 1000);
 
-// async function getChainId() {
-//   return ethers.provider.getNetwork().then((network) => network.chainId);
-// }
+async function getChainId() {
+  return ethers.provider.getNetwork().then((network) => network.chainId);
+}
 
-// describe("End-to-end", () => {
-//   let stack: Stack;
-//   let context: Context;
-//   let app: AppParserMock;
-//   let StackValue: StackValue__factory;
+// TODO: make more thorough end-to-end testing
+describe.only("End-to-end", () => {
+  let stack: Stack;
+  let ctx: Context;
+  let app: AppMock;
+  let StackValue: StackValue__factory;
 
-//   before(async () => {
-//     // Create StackValue Factory instance
-//     StackValue = await ethers.getContractFactory("StackValue");
+  before(async () => {
+    // Create StackValue Factory instance
+    StackValue = await ethers.getContractFactory("StackValue");
 
-//     // Deploy StringUtils library
-//     const stringLib = await (await ethers.getContractFactory("StringUtils")).deploy();
+    // Deploy StringUtils library
+    const stringLib = await (await ethers.getContractFactory("StringUtils")).deploy();
 
-//     // Deploy App
-//     const AppCont = await ethers.getContractFactory("AppParserMock", {
-//       libraries: { StringUtils: stringLib.address },
-//     });
-//     app = await AppCont.deploy();
+    // Deploy Parser
+    const ParserCont = await ethers.getContractFactory("Parser", {
+      libraries: { StringUtils: stringLib.address },
+    });
+    const parser = await ParserCont.deploy();
 
-//     // Create Context instance
-//     context = await ethers.getContractAt("Context", await app.ctx());
+    // Deploy Executor
+    const executor = await (await ethers.getContractFactory("Executor")).deploy(await parser.opcodes());
 
-//     // Create Stack instance
-//     const StackCont = await ethers.getContractFactory("Stack");
-//     const contextStackAddress = await context.stack();
-//     stack = StackCont.attach(contextStackAddress);
-//   });
+    // Deploy App
+    const AppCont = await ethers.getContractFactory("AppMock");
+    app = await AppCont.deploy(parser.address, executor.address);
+  });
 
-//   afterEach(async () => app.reset());
+  beforeEach(async () => {
+    // Deploy Context & setup
+    ctx = await (await ethers.getContractFactory("Context")).deploy();
 
-//   describe("blockChainId < loadLocal uint256 VAR", async () => {
-//     const chainId = await getChainId();
-//     const varHashPadded = hex4Bytes("VAR");
-//     const varHash = varHashPadded.slice(2, 2 + 8);
-//     const varValue = chainId + 1;
+    // Create Stack instance
+    const StackCont = await ethers.getContractFactory("Stack");
+    const contextStackAddress = await ctx.stack();
+    stack = StackCont.attach(contextStackAddress);
+  });
 
-//     const code = [["blockChainId"], ["loadLocal", "uint256", "VAR"], ["<"]];
+  describe("blockChainId < loadLocal uint256 VAR", () => {
+    it("blockChainId < loadLocal uint256 VAR", async () => {
+      const chainId = await getChainId();
+      const varHashPadded = hex4Bytes("VAR");
+      const varHash = varHashPadded.slice(2, 2 + 8);
+      const varValue = chainId + 1;
+      const code = "blockChainId < loadLocal uint256 VAR";
+      const bytecode = ["17", `1b01${varHash}`, "03"];
 
-//     const bytecode = ["17", `1b01${varHash}`, "03"];
+      // Parse code
+      await app.setStorageUint256(varHashPadded, varValue);
+      await app.parse(ctx.address, code);
 
-//     const stackStepByStep = [[chainId], [chainId, varValue], [1]];
+      const resultBytecode = await ctx.program();
+      expect(resultBytecode).to.be.equal(`0x${bytecode.join("")}`);
 
-//     it("bytecode", async () => {
-//       await app.parseCode(code.flat());
+      // Execute program
+      await app.execute(ctx.address);
 
-//       const resultBytecode = await context.program();
-//       expect(resultBytecode).to.be.equal(`0x${bytecode.join("")}`);
-//     });
+      await checkStack(StackValue, stack, 1, 1);
+    });
+  });
 
-//     it("step-by-step stack", async () => {
-//       await app.setStorageUint256(varHashPadded, varValue);
+  describe("((time > init) and (time < expiry)) or ((risk == true) == false)", async () => {
+    const ITS_RISKY = 1;
+    const NOT_RISKY = 0;
+    const NOW = Math.floor(Date.now() / 1000);
 
-//       for (let i = 0; i < stackStepByStep.length; i += 1) {
-//         const [targetStack, ops] = [stackStepByStep[i], code[i]];
-//         if (!targetStack || !ops) {
-//           throw new Error("Invalid bytecode or stack");
-//         }
+    const code = `
+      ((loadLocal uint256 NOW > loadLocal uint256 INIT)
+      and
+      (loadLocal uint256 NOW < loadLocal uint256 EXPIRY))
+      or
+      ((loadLocal bool RISK == bool true)
+      ==
+      (bool false))
+    `;
 
-//         await app.exec(ops);
-//         await checkStackTail(StackValue, stack, targetStack.length, targetStack);
-//       }
-//     });
-//   });
+    const bytecode = [
+      /* eslint-disable no-multi-spaces */
+      `1b01${hex4BytesShort("NOW")}`, // ['loadLocal', 'uint256', 'NOW'],
+      `1b01${hex4BytesShort("INIT")}`, // ['loadLocal', 'uint256', 'INIT'],
+      "04", // ['>'], // A
 
-//   describe("((time > init) and (time < expiry)) or ((risk == true) == false)", async () => {
-//     const ITS_RISKY = 1;
-//     const NOT_RISKY = 0;
-//     const NOW = Math.floor(Date.now() / 1000);
+      `1b01${hex4BytesShort("NOW")}`, // ['loadLocal', 'uint256', 'NOW'],
+      `1b01${hex4BytesShort("EXPIRY")}`, // ['loadLocal', 'uint256', 'EXPIRY'],
+      "03", // ['<'], // B
 
-//     // (A & B) | C
-//     const code = [
-//       ["loadLocal", "uint256", "NOW"],
-//       ["loadLocal", "uint256", "INIT"],
-//       [">"], // A
+      "12", // ['and'],
 
-//       ["loadLocal", "uint256", "NOW"],
-//       ["loadLocal", "uint256", "EXPIRY"],
-//       ["<"], // B
+      `1b02${hex4BytesShort("RISK")}`, // ['loadLocal', 'bool', 'RISK'],
+      "1801", // ['bool', 'true'],
+      "01", // ['=='],
 
-//       ["and"],
+      "1800", // ['bool', 'false'],
+      "01", // ['=='], // C
 
-//       ["loadLocal", "bool", "RISK"],
-//       ["bool", "true"],
-//       ["=="],
+      "13", // ['or'],
+      /* eslint-enable no-multi-spaces */
+    ];
 
-//       ["bool", "false"],
-//       ["=="], // C
+    async function testCase(
+      INIT: number,
+      EXPIRY: number,
+      RISK: number,
+      A: number,
+      B: number,
+      C: number,
+      result: number
+    ) {
+      await app.setStorageUint256(hex4Bytes("NOW"), NOW);
+      await app.setStorageUint256(hex4Bytes("INIT"), INIT);
+      await app.setStorageUint256(hex4Bytes("EXPIRY"), EXPIRY);
+      await app.setStorageUint256(hex4Bytes("RISK"), RISK);
 
-//       ["or"],
-//     ];
+      await app.execute(ctx.address);
+      await checkStack(StackValue, stack, 1, result);
+    }
 
-//     const bytecode = [
-//       /* eslint-disable no-multi-spaces */
-//       `1b01${hex4BytesShort("NOW")}`, // ['loadLocal', 'uint256', 'NOW'],
-//       `1b01${hex4BytesShort("INIT")}`, // ['loadLocal', 'uint256', 'INIT'],
-//       "04", // ['>'], // A
+    it("bytecode", async () => {
+      await app.parse(ctx.address, code);
 
-//       `1b01${hex4BytesShort("NOW")}`, // ['loadLocal', 'uint256', 'NOW'],
-//       `1b01${hex4BytesShort("EXPIRY")}`, // ['loadLocal', 'uint256', 'EXPIRY'],
-//       "03", // ['<'], // B
+      const resultBytecode = await ctx.program();
+      expect(resultBytecode).to.be.equal(`0x${bytecode.join("")}`);
+    });
 
-//       "12", // ['and'],
+    describe("step-by-step stack", async () => {
+      beforeEach(async () => {
+        await app.parse(ctx.address, code);
+      });
 
-//       `1b02${hex4BytesShort("RISK")}`, // ['loadLocal', 'bool', 'RISK'],
-//       "1801", // ['bool', 'true'],
-//       "01", // ['=='],
-
-//       "1800", // ['bool', 'false'],
-//       "01", // ['=='], // C
-
-//       "13", // ['or'],
-//       /* eslint-enable no-multi-spaces */
-//     ];
-
-//     async function testCase(
-//       INIT: number,
-//       EXPIRY: number,
-//       RISK: number,
-//       A: number,
-//       B: number,
-//       C: number,
-//       result: number,
-//     ) {
-//       const stackStepByStep = [
-//         /* eslint-disable no-multi-spaces */
-//         [NOW], // ['loadLocal', 'uint256', 'NOW'],
-//         [NOW, INIT], // ['loadLocal', 'uint256', 'INIT'],
-//         [A], // ['>'], // A
-
-//         [A, NOW], // ['loadLocal', 'uint256', 'NOW'],
-//         [A, NOW, EXPIRY], // ['loadLocal', 'uint256', 'EXPIRY'],
-//         [A, B], // ['<'], // B
-
-//         [+(A && B)], // ['and'],
-
-//         [+(A && B), RISK], // ['loadLocal', 'bool', 'RISK'],
-//         [+(A && B), RISK, 1], // ['bool', 'true'],
-//         [+(A && B), +(RISK === 1)], // ['=='],
-
-//         [+(A && B), +(RISK === 1), 0], // ['bool', 'false'],
-//         [+(A && B), C], // ['=='], // C
-
-//         [result], // ['or'],
-//         /* eslint-enable no-multi-spaces */
-//       ];
-
-//       await app.setStorageUint256(hex4Bytes("NOW"), NOW);
-//       await app.setStorageUint256(hex4Bytes("INIT"), INIT);
-//       await app.setStorageUint256(hex4Bytes("EXPIRY"), EXPIRY);
-//       await app.setStorageUint256(hex4Bytes("RISK"), RISK);
-
-//       for (let i = 0; i < stackStepByStep.length; i += 1) {
-//         const [targetStack, ops] = [stackStepByStep[i], code[i]];
-
-//         if (!targetStack || !ops) {
-//           throw new Error("Invalid bytecode or stack");
-//         }
-
-//         await app.exec(ops);
-//         await checkStackTail(StackValue, stack, targetStack.length, targetStack);
-//       }
-//     }
-
-//     it("bytecode", async () => {
-//       await app.parseCode(code.flat());
-
-//       const resultBytecode = await context.program();
-//       expect(resultBytecode).to.be.equal(`0x${bytecode.join("")}`);
-//     });
-
-//     describe("step-by-step stack", async () => {
-//       // T - true, F - false
-//       it("((T & T) | T) == T", async () => testCase(PREV_MONTH, NEXT_MONTH, NOT_RISKY, 1, 1, 1, 1));
-//       it("((T & F) | T) == T", async () => testCase(PREV_MONTH, PREV_MONTH, NOT_RISKY, 1, 0, 1, 1));
-//       it("((F & T) | T) == T", async () => testCase(NEXT_MONTH, NEXT_MONTH, NOT_RISKY, 0, 1, 1, 1));
-//       it("((F & F) | T) == T", async () => testCase(NEXT_MONTH, PREV_MONTH, NOT_RISKY, 0, 0, 1, 1));
-//       it("((T & T) | F) == T", async () => testCase(PREV_MONTH, NEXT_MONTH, ITS_RISKY, 1, 1, 0, 1));
-//       it("((T & F) | F) == F", async () => testCase(PREV_MONTH, PREV_MONTH, ITS_RISKY, 1, 0, 0, 0));
-//       it("((F & T) | F) == F", async () => testCase(NEXT_MONTH, NEXT_MONTH, ITS_RISKY, 0, 1, 0, 0));
-//       it("((F & F) | F) == F", async () => testCase(NEXT_MONTH, PREV_MONTH, ITS_RISKY, 0, 0, 0, 0));
-//     });
-//   });
-// });
+      // T - true, F - false
+      it("((T & T) | T) == T", async () => testCase(PREV_MONTH, NEXT_MONTH, NOT_RISKY, 1, 1, 1, 1));
+      it("((T & F) | T) == T", async () => testCase(PREV_MONTH, PREV_MONTH, NOT_RISKY, 1, 0, 1, 1));
+      it("((F & T) | T) == T", async () => testCase(NEXT_MONTH, NEXT_MONTH, NOT_RISKY, 0, 1, 1, 1));
+      it("((F & F) | T) == T", async () => testCase(NEXT_MONTH, PREV_MONTH, NOT_RISKY, 0, 0, 1, 1));
+      it("((T & T) | F) == T", async () => testCase(PREV_MONTH, NEXT_MONTH, ITS_RISKY, 1, 1, 0, 1));
+      it("((T & F) | F) == F", async () => testCase(PREV_MONTH, PREV_MONTH, ITS_RISKY, 1, 0, 0, 0));
+      it("((F & T) | F) == F", async () => testCase(NEXT_MONTH, NEXT_MONTH, ITS_RISKY, 0, 1, 0, 0));
+      it("((F & F) | F) == F", async () => testCase(NEXT_MONTH, PREV_MONTH, ITS_RISKY, 0, 0, 0, 0));
+    });
+  });
+});
