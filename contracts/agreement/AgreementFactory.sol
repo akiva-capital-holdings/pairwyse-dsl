248e93b2ae9e3622fa12a1c2fc88e387eb2c0470
Misha Kushka <kushka.misha@gmail.com>
2022-07-05 18:46:07 +0200

    More comments

--- contracts/agreement/AgreementFactory.sol
+++ contracts/agreement/AgreementFactory.sol
@@ -3,10 +3,18 @@ pragma solidity ^0.8.0;
 
 import { Agreement } from './Agreement.sol';
 
+/**
+ * @dev Factory to generate Agreement contract instances
+ */
 contract AgreementFactory {
     address[] public deployedAgreements;
     event NewAgreement(address agreement);
 
+    /**
+     * @dev Deploy new Agreement contract
+     * @param _parser Parser contract instance
+     * @return _agreementAddr Address of newly created Agreement
+     */
     function deployAgreement(address _parser) external returns (address _agreementAddr) {
         Agreement _agreement = new Agreement(_parser);
         _agreementAddr = address(_agreement);
@@ -14,6 +22,10 @@ contract AgreementFactory {
         emit NewAgreement(_agreementAddr);
     }
 
+    /**
+     * @dev Get length of the deployed agreement array
+     * @return The length of the array
+     */
     function getDeployedAgreementsLen() external view returns (uint256) {
         return deployedAgreements.length;
     }
