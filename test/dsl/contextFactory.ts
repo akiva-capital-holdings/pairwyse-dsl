f180b9af51a1509acd6a6d1ea3add143ad5fc8e4
0xEV <ev.monastyrna@outlook.com>
2022-08-17 00:59:09 -0400

    Fixed context Factory test. speed up with snapshots

--- test/dsl/contextFactory.ts
+++ test/dsl/contextFactory.ts
@@ -1,29 +1,37 @@
 import { expect } from 'chai';
-import { ethers } from 'hardhat';
-import { ContextFactory as ContextFactoryType } from '../../typechain-types';
+import { ethers, network } from 'hardhat';
+import { ContextFactory } from '../../typechain-types';
 
 describe('ContextFactory', () => {
-  let factory: ContextFactoryType;
+  let factory: ContextFactory;
+  let snapshotId: number;
 
-  beforeEach(async () => {
+  const agreementAddr = '0xc5a5C42992dECbae36851359345FE25997F5C42d'; // just example
+
+  before(async () => {
     // Deploy ContextFactory
     factory = await (await ethers.getContractFactory('ContextFactory')).deploy();
+    snapshotId = await network.provider.send('evm_snapshot');
+  });
+
+  afterEach(async () => {
+    await network.provider.send('evm_revert', [snapshotId]);
   });
 
   it('deploy context', async () => {
-    const returnedAddr = await factory.callStatic.deployContext();
-    await factory.deployContext();
+    const returnedAddr = await factory.callStatic.deployContext(agreementAddr);
+    await factory.deployContext(agreementAddr);
     const events = await factory.queryFilter(factory.filters.NewContext());
     const eventAddr = events[0].args.context;
     expect(returnedAddr).to.equal(eventAddr);
   });
 
   it('deployed length', async () => {
-    await factory.deployContext();
+    await factory.deployContext(agreementAddr);
     expect(await factory.getDeployedContextsLen()).to.equal(1);
-    await factory.deployContext();
+    await factory.deployContext(agreementAddr);
     expect(await factory.getDeployedContextsLen()).to.equal(2);
-    await factory.deployContext();
+    await factory.deployContext(agreementAddr);
     expect(await factory.getDeployedContextsLen()).to.equal(3);
   });
 });
