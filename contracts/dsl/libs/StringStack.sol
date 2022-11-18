777db946ec4a959779eb6a865132902c293aaa46
Misha Kushka <kushka.misha@gmail.com>
2022-11-18 15:53:52 +0100

    Add comments to StringStack

--- contracts/dsl/libs/StringStack.sol
+++ contracts/dsl/libs/StringStack.sol
@@ -5,33 +5,44 @@ import { ErrorsStack } from '../libs/Errors.sol';
 import { StringUtils } from './StringUtils.sol';
 
 // TODO: add tests for this file
+/**
+ * @dev This library has all the functions to use solidity string array as a struct
+ */
 library StringStack {
     using StringUtils for string;
 
     /**
      * @dev Push element to array in the first position
      * @dev As the array has fixed size, we drop the last element when addind a new one to the beginning of the array
+     * @param _stack String stack
+     * @param _element String to be added to the stack
      */
     function pushToStack(
         string[] memory _stack,
         string memory _element
     ) external pure returns (string[] memory) {
-        // console.log('push to stack');
         _stack[stackLength(_stack)] = _element;
         return _stack;
     }
 
+    /**
+     * @dev Removes the top element from the stack
+     * @param _stack String stack
+     */
     function popFromStack(
         string[] memory _stack
     ) external pure returns (string[] memory, string memory) {
-        // console.log('pop from stack');
         string memory _topElement = seeLastInStack(_stack);
         _stack[stackLength(_stack) - 1] = '';
         return (_stack, _topElement);
     }
 
+    /**
+     * @dev Returns the current length of stack (excluding empty strings!)
+     * @param _stack String stack
+     * @return The length of the stack excluding empty strings
+     */
     function stackLength(string[] memory _stack) public pure returns (uint256) {
-        // console.log('stackLength');
         uint256 i;
         while (!_stack[i].equal('')) {
             i++;
@@ -39,8 +50,12 @@ library StringStack {
         return i;
     }
 
+    /**
+     * @dev Returns the top element in the stack without removing it
+     * @param _stack String stack
+     * @return The top element in the stack
+     */
     function seeLastInStack(string[] memory _stack) public pure returns (string memory) {
-        // console.log('last in stack is:', _stack[stackLength(_stack) - 1]);
         uint256 _len = stackLength(_stack);
         require(_len > 0, ErrorsStack.STK4);
         return _stack[_len - 1];
