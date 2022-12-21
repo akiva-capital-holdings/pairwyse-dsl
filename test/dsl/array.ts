3b92cec6851a3dbbca76ccc933aff0d8512504d0
Misha Kushka <kushka.misha@gmail.com>
2022-12-21 15:07:55 +0100

    Add one more test

--- test/dsl/array.ts
+++ test/dsl/array.ts
@@ -4,7 +4,7 @@ import { Array } from '../../typechain-types';
 
 const { ethers, network } = hre;
 
-describe.only('Array', () => {
+describe('Array', () => {
   let array: Array;
   let snapshotId: number;
 
@@ -21,10 +21,22 @@ describe.only('Array', () => {
   });
 
   describe('set array head', () => {
-    it('success', async () => {
+    it('success #1', async () => {
       const isArray = true;
       const elemsType = 1; // type: number
       const nextPrt = '0x12345678';
+      await array.setStorage('NUMBERS', isArray, elemsType, nextPrt);
+      const [isArrayReal, elemsTypeReal, nextPrtReal] = await array.getStorage('NUMBERS');
+
+      expect(isArrayReal).equal(isArray);
+      expect(elemsTypeReal).equal(elemsType);
+      expect(nextPrtReal).equal(nextPrt);
+    });
+
+    it('success #2: empty parameters', async () => {
+      const isArray = false;
+      const elemsType = 0; // type: number
+      const nextPrt = '0x00000000';
       await array.setStorage('NUMBERS', isArray, elemsType, nextPrt);
       const [isArrayReal, elemsTypeReal, nextPrtReal] = await array.getStorage('NUMBERS');
 
