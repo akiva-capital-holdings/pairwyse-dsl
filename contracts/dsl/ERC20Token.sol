bb1f52efc5c3f5f2c7c4a0b28a7e6c00c9aff623
Даниил Овсянников <ovsuanikov@gmail.com>
2022-12-22 15:58:12 +0200

    for deploy erc20 from FE

---
+++ contracts/dsl/ERC20Token.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import { ERC20 } from './test/ERC20.sol';
+
+contract ERC20Token is ERC20 {
+    constructor(
+        string memory tokenName,
+        string memory tokenSymbol,
+        uint256 totalSuply
+    ) ERC20(tokenName, tokenSymbol) {
+        _mint(msg.sender, totalSuply);
+    }
+}
