f1f4da883ab8c4b67697c73fc15d0cab6f6520fe
Даниил Овсянников <ovsuanikov@gmail.com>
2022-12-29 19:02:12 +0200

    add governance bytecode

---
+++ scripts/contextBytecode.ts
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import * as hre from 'hardhat';
+
+async function main() {
+  console.log(`Deploying from address ${(await hre.ethers.getSigners())[0].address}`);
+
+  // Note: run this on the same node as Front End to actually deploy these libraries
+
+  const tokenContract = await hre.ethers.getContractFactory('Context');
+
+  fs.writeFileSync(
+    path.join(__dirname, '../bytecodeFolder/context.bytecode'),
+    tokenContract.bytecode
+  );
+}
+
+main();
