e8953c39bb2f747a7009f736406245ce996d4474
Misha Kushka <kushka.misha@gmail.com>
2023-01-11 11:16:08 +0100

    Multitranche test for step 2

--- test/agreement/multiTranche.ts
+++ test/agreement/multiTranche.ts
@@ -2,6 +2,7 @@ import * as hre from 'hardhat';
 import { expect } from 'chai';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { parseEther } from 'ethers/lib/utils';
+import { network } from 'hardhat';
 import { deployOpcodeLibs, deployPreprocessor } from '../../scripts/utils/deploy.utils';
 import { IERC20, MultiTranche } from '../../typechain-types';
 import { deployBaseMock } from '../../scripts/utils/deploy.utils.mock';
@@ -19,14 +20,16 @@ describe.only('Multi Tranche', () => {
   let investor1: SignerWithAddress;
   let investor2: SignerWithAddress;
   let investor3: SignerWithAddress;
+  let anyone: SignerWithAddress;
   let preprocessorAddr: string;
   let multiTranche: MultiTranche;
   let usdc: ERC20Mintable;
   let wusdc: IERC20;
+  let snapshotId: number;
   const [enterRecord, depositRecord, withdrawRecord] = [1, 2, 3];
 
   before(async () => {
-    [creator, investor1, investor2, investor3] = await ethers.getSigners();
+    [creator, investor1, investor2, investor3, anyone] = await ethers.getSigners();
 
     preprocessorAddr = await deployPreprocessor(hre);
 
@@ -63,16 +66,16 @@ describe.only('Multi Tranche', () => {
     wusdc = await ethers.getContractAt('IERC20', await multiTranche.wusdc());
 
     await multiTranche.setStorageAddress(hex4Bytes('USDC'), usdc.address);
-    // await multiTranche.setStorageAddress(hex4Bytes('ME'), usdc.address);
+    await parse(multiTranche, preprocessorAddr);
   });
 
-  // beforeEach(async () => {
-  //   snapshotId = await network.provider.send('evm_snapshot');
-  // });
+  beforeEach(async () => {
+    snapshotId = await network.provider.send('evm_snapshot');
+  });
 
-  // afterEach(async () => {
-  //   await network.provider.send('evm_revert', [snapshotId]);
-  // });
+  afterEach(async () => {
+    await network.provider.send('evm_revert', [snapshotId]);
+  });
 
   it.only('Step 1. Enter MultiTranche', async () => {
     await usdc.connect(investor1).approve(multiTranche.address, parseEther('100'));
@@ -82,11 +85,24 @@ describe.only('Multi Tranche', () => {
     expect(await usdc.balanceOf(multiTranche.address)).equal(parseEther('0'));
     expect(await wusdc.balanceOf(investor1.address)).equal(parseEther('0'));
 
-    await parse(multiTranche, preprocessorAddr);
     await multiTranche.connect(investor1).execute(enterRecord);
 
     expect(await usdc.balanceOf(investor1.address)).equal(parseEther('0'));
     expect(await usdc.balanceOf(multiTranche.address)).equal(parseEther('100'));
     expect(await wusdc.balanceOf(investor1.address)).equal(parseEther('100'));
+  });
+
+  it.only('Step 2. Deposit all USDC to Compound', async () => {
+    await usdc.mint(multiTranche.address, parseEther('1000'));
+
+    expect(await multiTranche.getStorageUint256(hex4Bytes('TOTAL_USDC')), '0');
+    // TODO: check Compound deposit before
+    await multiTranche.connect(anyone).execute(depositRecord);
+    // TODO: check Compound deposit after
+    expect(
+      await multiTranche.getStorageUint256(hex4Bytes('TOTAL_USDC')),
+      parseEther('1000').toString()
+    );
+    expect(await multiTranche.getStorageUint256(hex4Bytes('DEPOSIT_TIME'))).not.equal('0');
   });
 });
