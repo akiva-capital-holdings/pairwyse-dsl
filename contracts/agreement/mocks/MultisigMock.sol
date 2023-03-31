0700a69e9fa178d4f638f17bc787f17597fa6192
astronaut7 <3900266+astronaut7@users.noreply.github.com>
2023-03-31 09:51:36 -0400

    Update MultisigMock.sol

--- contracts/agreement/mocks/MultisigMock.sol
+++ contracts/agreement/mocks/MultisigMock.sol
@@ -1,5 +1,12 @@
-// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
+
+/**
+ * (c) 2023 Akiva Capital Holdings, LLC.  All Rights Reserved.
+ * 
+ * For LICENSE details, please visit:
+ * https://github.com/akiva-capital-holdings/pairwyse-dsl/blob/master/LICENSE
+ *
+ */
 
 /**
  * This is the contract that simulates Multisig. The contract just executes any transaction given to it without
