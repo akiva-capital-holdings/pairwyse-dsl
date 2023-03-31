5034ba7af4b1faab47a449fee1b149d5f3fcaeb7
astronaut7 <3900266+astronaut7@users.noreply.github.com>
2023-03-31 10:02:55 -0400

    Update IAgreement.sol

--- contracts/dsl/interfaces/IAgreement.sol
+++ contracts/dsl/interfaces/IAgreement.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.0;
 
 /**
- * (c) 2023 Akiva Capital Holdings, LLC.  All Rights Reserved.
+ * (c) 2023 Pairwyse Foundation.  All Rights Reserved.
  * 
  * For LICENSE details, please visit:
  * https://github.com/akiva-capital-holdings/pairwyse-dsl/blob/master/LICENSE
