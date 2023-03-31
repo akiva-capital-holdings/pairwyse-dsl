6a807c5dea894597188dd6d72b3d970a7b87c13f
astronaut7 <3900266+astronaut7@users.noreply.github.com>
2023-03-31 09:48:23 -0400

    Update Agreement.sol

--- contracts/agreement/Agreement.sol
+++ contracts/agreement/Agreement.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+/**
+ * (c) 2023 Akiva Capital Holdings, LLC.  All Rights Reserved.
+ * 
+ * For LICENSE details, please visit:
+ * https://github.com/akiva-capital-holdings/pairwyse-dsl/blob/master/LICENSE
+ *
+ */
+
 import { IAgreement } from '../dsl/interfaces/IAgreement.sol';
 import { IParser } from '../dsl/interfaces/IParser.sol';
 import { IDSLContext } from '../dsl/interfaces/IDSLContext.sol';
