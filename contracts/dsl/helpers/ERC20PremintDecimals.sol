e60a6be4a2b4fd862bb025e00d7f3afea827bb63
astronaut7 <3900266+astronaut7@users.noreply.github.com>
2023-03-31 09:55:28 -0400

    Update ERC20PremintDecimals.sol

--- contracts/dsl/helpers/ERC20PremintDecimals.sol
+++ contracts/dsl/helpers/ERC20PremintDecimals.sol
@@ -1,6 +1,13 @@
-// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+/**
+ * (c) 2023 Akiva Capital Holdings, LLC.  All Rights Reserved.
+ * 
+ * For LICENSE details, please visit:
+ * https://github.com/akiva-capital-holdings/pairwyse-dsl/blob/master/LICENSE
+ *
+ */
+ 
 import { ERC20 } from './ERC20.sol';
 
 contract ERC20PremintDecimals is ERC20 {
